ae80a3a231bb543076db2840d9848ee58ffc11e4
superwebdev19 <superwebdev19@gmail.com>
2020-03-12 12:19:36 -0600

    completed about us view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <Navbar />
+    <br /><br />
     <div class="container">
       <router-view />
     </div>
   </div>
 </template>
+
 <script>
 import Navbar from "@/components/Navbar";
 export default {
@@ -14,4 +16,9 @@ export default {
   }
 };
 </script>
-<style></style>
+
+<style>
+.card {
+  border-radius: 0px;
+}
+</style>
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="display3">About Us</h1>
+    <div class="card p-3">
+      <h4>This is MEVN project</h4>
+      <p>Thank you!</p>
+    </div>
   </div>
 </template>
