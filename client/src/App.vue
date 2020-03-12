ef809a7980e0874df39eb605ff82dbd481fb6cc5
superwebdev19 <superwebdev19@gmail.com>
2020-03-11 20:46:39 -0600

    Updated navbar and added some views

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <Navbar />
     <!-- <div id="nav">
-      <router-link to="/">Home</router-link>|
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>-->
     <router-view />
